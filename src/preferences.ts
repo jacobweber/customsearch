964060518e2fcaf5ec17ef76724c756bf68dbbde
Jacob Weber <jacob@jacobweber.com>
2020-03-06 12:10:09 -0500

    always reload search types when saving prefs, in case contents changed

--- src/preferences.ts
+++ src/preferences.ts
@@ -132,9 +132,7 @@ export const loadSearchTypes = async function(searchTypesPath: string): Promise<
 };
 
 export const savePreferences = async function(data: Preferences = {}, oldData: Preferences = null): Promise<Preferences> {
-	const searchTypes = oldData.searchTypesPath === data.searchTypesPath
-		? oldData.searchTypes
-		: await loadSearchTypes(data.searchTypesPath);
+	const searchTypes = await loadSearchTypes(data.searchTypesPath);
 	const customParams: CustomParamsMap = {};
 	for (const searchType of searchTypes) {
 		if (searchType.customParams) {
