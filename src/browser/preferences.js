dcaee239ae3455bd5652bfce104a2599e88b828b
Jacob Weber <jacob@jacobweber.com>
2020-03-06 13:25:32 -0500

    fix prefs label

--- src/browser/preferences.js
+++ src/browser/preferences.js
@@ -120,7 +120,7 @@
 				else if (!param.password && param.default !== undefined) value = param.default;
 				const html = `<div class="row custom-params-row">
 					<input type="hidden" class="custom-params-id" value="${htmlEscape(fullID)}" />
-					<label class="label" for="custom-params-value-${idx}">${htmlEscape(searchType.id + ' ' + param.label)}:</label>
+					<label class="label" for="custom-params-value-${idx}">${htmlEscape(searchType.label + ' ' + param.label)}:</label>
 					<input type="${param.password ? 'password' : 'text'}" class="text-field custom-params-value" id="custom-params-value-${idx}" value="${htmlEscape(value)}" />
 				</div>`;
 				parent.insertAdjacentHTML('beforeend', html);
